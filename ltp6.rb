ac942ee0a75d82bc93290e6104113adc4727118f
L7csa <sa4soto@yahoo.com>
2013-09-21 02:09:11 -0700

    Create ltp6.rb

---
+++ ltp6.rb
@@ -0,0 +1,15 @@
+# angry boss
+
+puts "What do you want!?"
+answer = gets.chomp
+puts "Whaddaya mean \"#{answer}\"?!? You\'re fired!!"
+
+
+# table of contents
+
+puts "Table of Contents".center(70)
+puts
+puts "Chapter 1: Getting Started".ljust(35) + "page 1".rjust(35)
+puts "Chapter 2: Numbers".ljust(35) + "page 9".rjust(35)
+puts "Chapter 3: Letters".ljust(35) + "page 13".rjust(35)
+
